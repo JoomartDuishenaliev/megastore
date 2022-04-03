0c4b76bdafacd479c7ea5666e439860c3b7b67f0
z to zee h <zhoomart.d@gmail.com>
2022-04-04 04:19:30 +0600

    feat: added notification

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const btn = document.querySelector('#nextLink');
+const notification = document.querySelector('.notification');
 
 btn.onclick = () => {
     let surname = document.querySelector('#mandatoryInfo');
@@ -17,7 +18,19 @@ btn.onclick = () => {
 
     if (storage.lastName.length === 0 || storage.firstName.length === 0 || storage.passport.length === 0 ||
         storage.IdNumber.length === 0 || storage.issueDate.length === 0) {
-        alert('fill in all');
+        notification.className = 'alert alert-warning col fw-bold text-center text-primary fs-5';
+        notification.style.height = '80px';
+        notification.style.width = '470px';
+        notification.style.position = 'absolute';
+        notification.style.zIndex = '999';
+        notification.textContent = 'Заполните необходимые поля';
+
+        let timeoutId = setTimeout(() => {
+            notification.className = '';
+            notification.textContent = '';
+            clearTimeout(timeoutId);
+        }, 2000)
+
         return btn.href = '#';
     } else {
         localStorage.setItem('dataStorage', JSON.stringify(storage));
